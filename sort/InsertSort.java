641b8db85265d63c5e7cce13918318b52d60049f
Thiago Roberto Queiroz <thigorqueiroz@gmail.com>
2020-06-18 12:36:44 -0300

    fix InsertSort

--- sort/InsertSort.java
+++ sort/InsertSort.java
@@ -1,23 +1,22 @@
-import java.util.*;
+import java.util.Arrays;
 
 public class InsertSort {
 
 	static void sort(int[] arr) {
 
 		for (int j = 1; j < arr.length; j++) {
-			int currentElement = arr[j];
-			int pivot = j - 1;
-			int previousElement =  arr[pivot];
-			while (pivot >= 0 && previousElement > currentElement) {
-				arr[pivot+1] = arr[pivot];
-				pivot--;
+			int key = arr[j];
+			int i = j - 1; //sempre movimenta o para posição anterior
+			while (i >= 0 && arr[i] > key) {
+				arr[i+1] = arr[i];
+				i--;
 			}
-			arr[pivot+1] = currentElement;
+			arr[i+1] = key;
 		}
 	}
 
 	public static void main(String[] args) {
-		int[] arr = new int [] {3,9,8,7,6,5,4};
+		int[] arr = new int [] {9,8,7,6,1,2,5,4};
 		sort(arr);
 		System.out.println(Arrays.toString(arr));
 	}
